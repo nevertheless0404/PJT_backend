db7298994251c78476afff236b53e63dcf49c307
kimdakyeom <74820869+kimdakyeom@users.noreply.github.com>
2022-12-01 02:47:35 +0900

    fix: allauth 소셜로그인 오류 해결

--- project/settings.py
+++ project/settings.py
@@ -62,7 +62,7 @@ AUTHENTICATION_BACKENDS = (
     'django.contrib.auth.backends.ModelBackend',
     'allauth.account.auth_backends.AuthenticationBackend',
 )
-
+#
 SITE_ID = 1
 ACCOUNT_UNIQUE_EMAIL = True
 ACCOUNT_USER_MODEL_USERNAME_FIELD = None
