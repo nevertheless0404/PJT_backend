5c623c2b2835187f8c156cc8c741b5b7cb3cac96
tenedict <tenedict@gmail.com>
2022-12-03 17:56:13 +0900

    bug: 투두리스트 성공인데 디테일이 안들어가짐 ㅜ

--- projects/views.py
+++ projects/views.py
@@ -80,21 +80,27 @@ class Projectdetail(APIView):
         project = self.get_object(pk)
         serializer = ProjectSerializer(project, data=request.data)
         members = Members.objects.filter(project=project.pk)
+        lead = 0
         for member in members:
             print('다영사랑해 다영사랑해 다영사랑해')
-            if member.user == request.user.email:
-                if serializer.is_valid():
-                    serializer.save()
-                    return Response(serializer.data)
+            if member.leader == 1:
+                lead = member
+                if lead.user == request.user.email:
+                    if serializer.is_valid():
+                        serializer.save()
+                        return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # project 삭제하기
     def delete(self, request, pk, format=None):
         project = self.get_object(pk)
         members = Members.objects.filter(project=project.pk)
+        lead = 0
         for member in members:
-            if member.user == request.user.email:
-                project.delete()
+            if member.leader == 1:
+                lead = member
+                if lead.user == request.user.email:
+                    project.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
 
 
@@ -126,21 +132,33 @@ def changeleader(request, project_pk, leader_pk, format=None):
 
 # todo의 목록을 보여주는 역할
 class Todolist(APIView):
-    permissions_classes = [IsAuthenticated]
+    # permissions_classes = [IsAuthenticated]
     # todo list를 보여줄 때
-    def get(self, request):
-        todos = Todo.objects.all()
+    def get(self, request, project_pk):
+        project = Project.objects.get(pk=project_pk)
+        members = Members.objects.filter(project=project)
+        print(members)
+        for member in members:
+            if request.user.email == member.user:
+                todos = Todo.objects.filter(project=project)
         # 여러 개의 객체를 serialization하기 위해 many=True로 설정
-        serializer = TodoSerializer(todos, many=True)
-        return Response(serializer.data)
+                serializer = TodoSerializer(todos, many=True)
+                return Response(serializer.data)
+        return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # 새로운 todo 글을 작성할 때
-    def post(self, request):
+    def post(self, request,  project_pk):
         # request.data는 사용자의 입력 데이터
+        project = Project.objects.get(pk=project_pk)
         serializer = TodoSerializer(data=request.data)
-        if serializer.is_valid():  # 유효성 검사
-            serializer.save()  # 저장
-            return Response(serializer.data, status=status.HTTP_201_CREATED)
+        members = Members.objects.filter(project=project)
+        for member in members:
+            if request.user.email == member.user:
+                if serializer.is_valid():  # 유효성 검사
+                    serializer.validated_data['project'] = project
+                    serializer.validated_data['user'] = request.user
+                    serializer.save()  # 저장
+                    return Response(serializer.data, status=status.HTTP_201_CREATED)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 
@@ -148,29 +166,36 @@ class Todolist(APIView):
 class Tododetail(APIView):
     permissions_classes = [IsAuthenticated]
     # todo 객체 가져오기
-    def get_object(self, pk):
+    def get_object(self, project_pk, pk):
         try:
             return Todo.objects.get(pk=pk)
         except Todo.DoesNotExist:
             raise Http404
 
     # todo의 detail 보기
-    def get(self, request, pk, format=None):
-        todo = self.get_object(pk)
-        serializer = TodoSerializer(todo)
-        return Response(serializer.data)
+    def get(self, request, project_pk, pk, format=None):
+        project = Project.objects.get(pk=project_pk)
+        serializer = TodoSerializer(data=request.data)
+        members = Members.objects.filter(project=project)
+        for member in members:
+            if request.user.email == member.user:
+                todo = self.get_object(pk)
+                serializer = TodoSerializer(todo)
+                return Response(serializer.data)
 
     # todo 수정하기
-    def put(self, request, pk, format=None):
+    def put(self, request, project_pk,  pk, format=None):
+        project = Project.objects.get(pk=project_pk)
         todo = self.get_object(pk)
         serializer = TodoSerializer(todo, data=request.data)
-        if serializer.is_valid():
-            serializer.save()
-            return Response(serializer.data)
+        if todo.user == request.user:
+            if serializer.is_valid():
+                serializer.save()
+                return Response(serializer.data)
         return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
     # todo 삭제하기
-    def delete(self, request, pk, format=None):
+    def delete(self, request, project_pk, pk, format=None):
         todo = self.get_object(pk)
         todo.delete()
         return Response(status=status.HTTP_204_NO_CONTENT)
@@ -254,14 +279,14 @@ class Membersadm(APIView):
         # return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
 
 @api_view(['GET'])
-def comment(request, todo_pk):
+def comment(request,  project_pk, todo_pk):
     serializer = CommentSerializer(data=request.data)
     comments = Comment.objects.filter(todo_id=todo_pk)
     serializer = CommentSerializer(comments, many=True)
     return Response(serializer.data)
 
 @api_view(['POST'])
-def comment_create(request, todo_pk):
+def comment_create(request, project_pk, todo_pk):
     serializer = CommentSerializer(data=request.data)
     todo = get_object_or_404(Todo, pk=todo_pk)
     if serializer.is_valid(raise_exception=True):
@@ -269,7 +294,7 @@ def comment_create(request, todo_pk):
         return Response(serializer.data)
 
 @api_view(['PUT','DELETE'])
-def comment_update_and_delete(request, todo_pk, comment_pk):
+def comment_update_and_delete(request, project_pk, todo_pk, comment_pk):
     comment = get_object_or_404(Comment, pk=comment_pk)
     if request.method == 'PUT':
         serializer = CommentSerializer(data=request.data, instance=comment)
